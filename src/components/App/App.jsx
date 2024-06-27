bbdbca20fd5c22d54ba7f6d0af435d2dd4b56130
Kalia Xiong <kaliaxyooj@gmail.com>
2024-06-26 21:13:36 -0500

    added some starter code to index.jsx and app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
+
+  const airlineList = useSelector(store => store.airline);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('adding airline', )
+
+    dispatch({type: 'UPDATE_AIRLINE', payload: newAirline});
+  }
 
   return (
     <div>
       <h1>Redux Airport</h1>
       <input placeholder='Airline Name' />
-      <button>Add Airline</button>
+      <button onSubmit={(e)=> handleSubmit(e)}>Add Airline</button>
       <table>{/* Airlines should be listed here */}</table>
+
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
-/** TODO: import REDUX **/
 
+
+
+/** TODO: import REDUX **/
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
 /** TODO: Add REDUCERS */
-
+ const airline = (state, action) => {
+    if (action.type === 'ADD_AIRLINE') {
+        console.log('airline added', airline);
+        let newAirline = action.payload;
+        return ;
+    }
+    return state;
+ }
 
 /** TODO: Create store */
+const storeInstance = createStore(
 
+    combineReducers(
+        { airline
+
+        }
+    )
+);
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store={storeInstance}>
         <App />
+        </Provider>
     </React.StrictMode>
 );
